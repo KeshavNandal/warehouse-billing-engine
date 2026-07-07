1e227ae44f84ae8f9b45fcca70dc1afb06e5436d
Keshav Kumar <keshav1132.becse24@chitkara.edu.in>
2026-07-07 17:32:51 +0530

    Feat: Implement and successfully test product selection retrieval via ResultSet

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,15 +1,31 @@
 import dao.ProductDAO;
 import model.Product;
 
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("=== Warehouse Billing Engine: Testing Database Insertion ===");
+        System.out.println("=== Warehouse Billing Engine: Fetching Inventory ===");
 
-        Product testProduct = new Product("Logitech G Pro Keyboard", "KB-LOGI-GPRO-02", 8999.00, 45);
         ProductDAO productDAO = new ProductDAO();
+        List <Product> inventory = productDAO.getAllProducts();
 
-        System.out.println("Attempting to save product: " + testProduct.getName() + "...");
-        productDAO.saveProduct(testProduct);
+        if(inventory.isEmpty())
+        {
+            System.out.println("No products found in the database table.");
+        }
+        else
+        {
+            System.out.println("Successfully pulled " + inventory.size() + " items from MySQL:\n");
+        }
+        for (Product p : inventory) {
+            System.out.println("----------------------------------------");
+            System.out.println("ID:             " + p.getId());
+            System.out.println("Product Name:   " + p.getName());
+            System.out.println("SKU Identifier: " + p.getSku());
+            System.out.println("Retail Price:   ₹" + p.getPrice());
+            System.out.println("Stock Level:    " + p.getStockQuantity() + " units");
+        }
 
         System.out.println("==========================================================");
     }
--- src/main/java/dao/ProductDAO.java
+++ src/main/java/dao/ProductDAO.java
@@ -1,10 +1,14 @@
 package dao;
+import com.mysql.cj.exceptions.ConnectionIsClosedException;
 import db.DBConnection;
 import model.Product;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class ProductDAO {
@@ -41,5 +45,54 @@ public class ProductDAO {
             }
         }
 
+    }
+
+    public List<Product> getAllProducts()
+    {
+        List<Product> productList = new ArrayList<>();
+        String sql = "SELECT * FROM products";
+
+        Connection conn = null;
+        PreparedStatement stmt = null;
+        ResultSet rs = null;
+
+        try
+        {
+            conn = DBConnection.getConnection();
+            stmt = conn.prepareStatement(sql);
+            rs = stmt.executeQuery();
+
+            while(rs.next())
+            {
+                int id = rs.getInt("id");
+                String name = rs.getString("name");
+                String sku = rs.getString("sku");
+                double price = rs.getDouble("price");
+                int stockQuantity = rs.getInt("stock_quantity");
+
+                Product product = new Product(id,name,sku,price,stockQuantity);
+                productList.add(product);
+            }
+        }
+        catch (SQLException e)
+        {
+            System.out.println("Error fetching products: " + e.getMessage());
+        }
+        finally
+        {
+            try
+            {
+                if (rs != null) rs.close();
+                if (stmt != null) stmt.close();
+                if (conn != null) conn.close();
+
+            }
+            catch (SQLException e)
+            {
+                System.out.println("Error closing database: " + e.getMessage());
+            }
+        }
+
+    return productList;
     }
 }
